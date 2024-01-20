3ddfb5b8379d49d230c1ce1f81c09acae7c64db1
vGuertl <veronika.guertl@gmail.com>
2024-01-20 19:43:16 +0100

    CalculatorImpl:  adapted for dotproduct function
     pop() only gets called within each case
     therefore E2E Tests for sin and cos adapted

--- src/main/java/at/campus02/swe/Calculator.java
+++ src/main/java/at/campus02/swe/Calculator.java
@@ -3,7 +3,7 @@ package at.campus02.swe;
 public interface Calculator {
 
     enum Operation {
-        add, sub, mul, div, mod, sin, cos, random
+        add, sub, mul, div, mod, sin, cos, random, dotproduct
     };
 
     void push(double value);
--- src/main/java/at/campus02/swe/logic/CalculatorImpl.java
+++ src/main/java/at/campus02/swe/logic/CalculatorImpl.java
@@ -13,31 +13,59 @@ public class CalculatorImpl implements Calculator {
     @Override
     public double perform(Operation op) throws CalculatorException {
 
-        double b = pop();
-        double a = pop();
-
+        double b;
+        double a;
         switch (op) {
             case add:
+                b = pop();
+                a = pop();
                 return a + b;
             case sub:
+                b = pop();
+                a = pop();
                 return a - b;
             case div:
+                b = pop();
+                a = pop();
                 double c = a / b;
                 if (Double.isInfinite(c))
                     throw new CalculatorException("Division by zero");
                 return c;
             case mul:
+                b = pop();
+                a = pop();
                 return a * b;
             case mod:
+                b = pop();
+                a = pop();
                 if (b == 0)
                     throw new CalculatorException("Division by zero");
                 return a % b;
             case sin:
+                a = pop();
                return Math.sin(Math.toRadians(a));
             case cos:
+                a = pop();
                 return Math.cos(Math.toRadians(a));
             case random:
+                b = pop();
+                a = pop();
                 return (int) ((Math.random() * (a - b)) + b);
+            case dotproduct:
+                int amount = (int) pop();
+                double result = 0;
+                double[] vectorA = new double[amount-1];
+                double[] vectorB = new double[amount-1];
+                for (int i = 0; i  < amount-1; i++) {
+                    vectorA[i] = pop();
+                }
+                for (int i = 0; i  < amount-1; i++) {
+                    vectorB[i] = pop();
+                }
+                for (int i = 0; i < amount-1; i++) {
+                    result += vectorA[i]*vectorB[i];
+                }
+                return result;
         }
         return 0;
     }
